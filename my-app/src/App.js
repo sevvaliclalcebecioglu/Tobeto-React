b2538e038dde6fa91e92c2dbd2e057e2bdb72101
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-05-28 15:26:41 +0300

    component oluşturma

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
+   /* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,18 @@ function App() {
           Learn React
         </a>
       </header>
+    </div> */
+
+   
+    <div>
+      <Header />
+      <p>Hello World!</p>  
+      <Header />
+      <p>Hello World!</p>  
+      <Header />
+      <p>Hello World!</p>  
     </div>
+
   );
 }
 
---
+++ my-app/src/components/header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>React JS Component</h1>
+    );
+}
+
+export default Header;
